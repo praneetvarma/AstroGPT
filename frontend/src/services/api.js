942d1f1aa4780fce125739a15a7401642e02a4e6
Praneet <praneetvarma45@gmail.com>
2026-07-23 21:37:40 +0530

    Update API URL to Render

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://astrogpt-api.onrender.com",
+  baseURL: "https://astrogpt-api.onrender.com",
+  timeout: 120000,
 });
 
 export default api;
